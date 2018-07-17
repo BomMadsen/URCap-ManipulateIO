16240de6bf8ee59caafb3e0ed3b797f6cc373859
BomMadsen <jacobbsmadsen@gmail.com>
2018-07-17 14:46:58 +0200

    Created View and SwingService classes

---
+++ com.ur.urcap.sample.manipulateIO/src/main/java/com/ur/urcap/sample/manipulateIO/impl/ManipulateProgramNodeView.java
@@ -0,0 +1,7 @@
+package com.ur.urcap.sample.manipulateIO.impl;
+
+import com.ur.urcap.api.contribution.program.swing.SwingProgramNodeView;
+
+public class ManipulateProgramNodeView implements SwingProgramNodeView<ManipulateProgramContribution> {
+
+}
---
+++ com.ur.urcap.sample.manipulateIO/src/main/java/com/ur/urcap/sample/manipulateIO/impl/ManipulateSwingProgramNodeService.java
@@ -0,0 +1,7 @@
+package com.ur.urcap.sample.manipulateIO.impl;
+
+import com.ur.urcap.api.contribution.program.swing.SwingProgramNodeService;
+
+public class ManipulateSwingProgramNodeService implements SwingProgramNodeService<ManipulateProgramContribution, ManipulateProgramNodeView> {
+
+}
