e65dd1b226978a0164a525d9b7ae2a46edd53f17
BomMadsen <jacobbsmadsen@gmail.com>
2018-07-17 14:48:10 +0200

    Added required methods for SwingProgramNodeService

--- com.ur.urcap.sample.manipulateIO/src/main/java/com/ur/urcap/sample/manipulateIO/impl/ManipulateSwingProgramNodeService.java
+++ com.ur.urcap.sample.manipulateIO/src/main/java/com/ur/urcap/sample/manipulateIO/impl/ManipulateSwingProgramNodeService.java
@@ -1,7 +1,45 @@
 package com.ur.urcap.sample.manipulateIO.impl;
 
+import java.util.Locale;
+
+import com.ur.urcap.api.contribution.ViewAPIProvider;
+import com.ur.urcap.api.contribution.program.ContributionConfiguration;
+import com.ur.urcap.api.contribution.program.CreationContext;
+import com.ur.urcap.api.contribution.program.ProgramAPIProvider;
 import com.ur.urcap.api.contribution.program.swing.SwingProgramNodeService;
+import com.ur.urcap.api.domain.data.DataModel;
 
 public class ManipulateSwingProgramNodeService implements SwingProgramNodeService<ManipulateProgramContribution, ManipulateProgramNodeView> {
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void configureContribution(ContributionConfiguration configuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ManipulateProgramNodeView createView(ViewAPIProvider apiProvider) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ManipulateProgramContribution createNode(ProgramAPIProvider apiProvider, ManipulateProgramNodeView view,
+			DataModel model, CreationContext context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
