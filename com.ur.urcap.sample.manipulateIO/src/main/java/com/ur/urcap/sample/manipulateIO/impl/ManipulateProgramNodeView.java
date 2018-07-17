9dce62ab3bcf8b09d522f931e322bb26df00995e
BomMadsen <jacobbsmadsen@gmail.com>
2018-07-17 14:52:59 +0200

    Added constructor to get ViewAPIProvider

--- com.ur.urcap.sample.manipulateIO/src/main/java/com/ur/urcap/sample/manipulateIO/impl/ManipulateProgramNodeView.java
+++ com.ur.urcap.sample.manipulateIO/src/main/java/com/ur/urcap/sample/manipulateIO/impl/ManipulateProgramNodeView.java
@@ -3,10 +3,17 @@ package com.ur.urcap.sample.manipulateIO.impl;
 import javax.swing.JPanel;
 
 import com.ur.urcap.api.contribution.ContributionProvider;
+import com.ur.urcap.api.contribution.ViewAPIProvider;
 import com.ur.urcap.api.contribution.program.swing.SwingProgramNodeView;
 
 public class ManipulateProgramNodeView implements SwingProgramNodeView<ManipulateProgramContribution> {
 
+	private final ViewAPIProvider apiProvider;
+	
+	public ManipulateProgramNodeView(ViewAPIProvider apiProvider) {
+		this.apiProvider = apiProvider;
+	}
+	
 	@Override
 	public void buildUI(JPanel panel, ContributionProvider<ManipulateProgramContribution> provider) {
 		// TODO Auto-generated method stub
