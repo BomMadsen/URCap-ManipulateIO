b58055a1dbc03200fd6c9bb64db6c93d1feef55d
BomMadsen <jacobbsmadsen@gmail.com>
2018-07-17 14:48:24 +0200

    Added required methods for View class

--- com.ur.urcap.sample.manipulateIO/src/main/java/com/ur/urcap/sample/manipulateIO/impl/ManipulateProgramNodeView.java
+++ com.ur.urcap.sample.manipulateIO/src/main/java/com/ur/urcap/sample/manipulateIO/impl/ManipulateProgramNodeView.java
@@ -1,7 +1,16 @@
 package com.ur.urcap.sample.manipulateIO.impl;
 
+import javax.swing.JPanel;
+
+import com.ur.urcap.api.contribution.ContributionProvider;
 import com.ur.urcap.api.contribution.program.swing.SwingProgramNodeView;
 
 public class ManipulateProgramNodeView implements SwingProgramNodeView<ManipulateProgramContribution> {
+
+	@Override
+	public void buildUI(JPanel panel, ContributionProvider<ManipulateProgramContribution> provider) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
