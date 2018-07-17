da313c46d8c6ce168d3b6d0cfebcb7b9547177ff
BomMadsen <jacobbsmadsen@gmail.com>
2018-07-17 15:56:05 +0200

    Added description labels to ProgramNodeView

--- com.ur.urcap.sample.manipulateIO/src/main/java/com/ur/urcap/sample/manipulateIO/impl/ManipulateProgramNodeView.java
+++ com.ur.urcap.sample.manipulateIO/src/main/java/com/ur/urcap/sample/manipulateIO/impl/ManipulateProgramNodeView.java
@@ -1,5 +1,8 @@
 package com.ur.urcap.sample.manipulateIO.impl;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.ur.urcap.api.contribution.ContributionProvider;
@@ -16,8 +19,24 @@ public class ManipulateProgramNodeView implements SwingProgramNodeView<Manipulat
 	
 	@Override
 	public void buildUI(JPanel panel, ContributionProvider<ManipulateProgramContribution> provider) {
-		// TODO Auto-generated method stub
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		panel.add(createDescriptionLabel("This node can interact with a fictive gripper."));
+		panel.add(createDescriptionLabel("The \"gripper\" grips, by toggling tool_out[0] high, and releases by setting tool_out[1] high."));
+		panel.add(createDescriptionLabel("tool_in[0] reports, that the gripper is currently holding an item, and tool_in[1] reports an error"));
+		panel.add(createDescriptionLabel("Gripper live position is expressed by tool analog in 0."));
+		
 		
 	}
-
+	
+	private Box createDescriptionLabel(String text) {
+		Box box = Box.createHorizontalBox();
+		
+		JLabel label = new JLabel(text);
+		
+		box.add(label);
+		
+		return box;
+	}
+	
 }
